1958d9f6510b56a1518fe0f04fa8f70faf217816
brunoricciotti <bricciotti@frba.utn.edu.ar>
2022-09-03 01:43:15 -0300

    ts8

--- Programas_python/analog_to_dig_filter.py
+++ Programas_python/analog_to_dig_filter.py
@@ -15,10 +15,10 @@ import matplotlib.pyplot as plt
 from splane import analyze_sys, pretty_print_bicuad_omegayq
 
 
-wo = 2*np.pi*6e3
+wo =2*np.pi*6e3
 fs = 100e3 #frecuencia de muestreo
 
-num_s = [wo**2] #numerador de filtro analogico
+num_s = [1,0,0] #numerador de filtro analogico
 den_s = [1,wo*np.sqrt(2),wo**2] #denominador de filtro analogico
 
 num_z,den_z = sig.bilinear(num_s,den_s,fs = fs) #transformo a digital
