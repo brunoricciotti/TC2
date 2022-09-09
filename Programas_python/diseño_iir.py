51610273a00325a8fa372279622ac98057605357
brunoricciotti <bricciotti@frba.utn.edu.ar>
2022-09-09 17:23:47 -0300

    ts9

---
+++ Programas_python/diseño_iir.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 19:37:54 2022
+
+@author: bruno
+"""
+
+# Inicialización e importación de módulos
+
+# Módulos para Jupyter
+import warnings
+warnings.filterwarnings('ignore')
+
+# Módulos importantantes
+import scipy.signal as sig
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from splane import pzmap, GroupDelay, bodePlot
+from scipy.signal import TransferFunction
+
+fig_sz_x = 10
+fig_sz_y = 7
+fig_dpi = 100 # dpi
+
+fig_font_size = 16
+
+mpl.rcParams['figure.figsize'] = (fig_sz_x,fig_sz_y)
+plt.rcParams.update({'font.size':fig_font_size})
+
+
+fs = 1000 # Hz
+nyq_frec = fs / 2
+
+# Plantilla
+
+# filter design
+ripple = 0.5 # dB
+atenuacion = 40 # dB
+
+ws1 = 1.0 #Hz
+wp1 = 3.0 #Hz
+wp2 = 25.0 #Hz
+ws2 = 35.0 #Hz
+
+frecs = np.array([0.0,         ws1,         wp1,     wp2,     ws2,         nyq_frec   ]) / nyq_frec
+gains = np.array([-atenuacion, -atenuacion, -ripple, -ripple, -atenuacion, -atenuacion])
+gains = 10**(gains/20)
+
+bp_sos_butter = sig.iirdesign([frecs[2],frecs[3]],[frecs[1],frecs[4]],ripple,atenuacion,analog=False,ftype='butter',output='sos',fs=fs)
+
+#w  = np.append(np.logspace(-1, 0.8, 250), np.logspace(0.9, 1.6, 250) )
+w  = np.append(np.logspace(-1, 0.8, 250), np.logspace(0.9, 1.6, 250) )
+w  = np.append(w, np.linspace(110, nyq_frec, 100, endpoint=True) ) / nyq_frec * np.pi
+
+H = sig.sosfreqz(bp_sos_butter, w)
+
+w = w / np.pi * nyq_frec
+
+plt.figure()
+
+plt.plot(w, 20*np.log10(np.abs(H)+1e-12), label='IIR-Butter {:d}'.format(bp_sos_butter.shape[0]*2) )
+  
+# cant_coeficientes = 1501
+
+# num_win = sig.firwin2(cant_coeficientes, frecs, gains , window='blackmanharris' )
+
+# den = 1.0
+
+# # muestreo el filtro donde me interesa verlo según la plantilla.
+# w  = np.append(np.logspace(-1, 0.8, 250), np.logspace(0.9, 1.6, 250) )
+# w  = np.append(w, np.linspace(110, nyq_frec, 100, endpoint=True) ) / nyq_frec * np.pi
+
+# hh_win = sig.freqz(num_win, den, w)
+
+# # renormalizo el eje de frecuencia
+# w = w / np.pi * nyq_frec
+
+# plt.plot(w, 20 * np.log10(abs(hh_win)), label='FIR-Win {:d}'.format(num_win.shape[0]))
+
+# plt.title('Filtros diseñados')
+# plt.xlabel('Frecuencia [Hz]')
+# plt.ylabel('Módulo [dB]')
+# plt.grid()
+# plt.axis([0, 100, -60, 5 ]);
+
+# axes_hdl = plt.gca()
+# axes_hdl.legend()
+
+# plot_plantilla(filter_type = 'bandpass', fpass = frecs[[2, 3]]* nyq_frec, ripple = ripple , fstop = frecs[ [1, 4] ]* nyq_frec, attenuation = atenuacion, fs = fs)
